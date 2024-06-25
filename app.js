2913dbae99ff30b681dcfca47e09fefc0d447b19
rohith1005H <rohithk1104@gmail.com>
2024-06-25 11:50:01 +0530

    tried using branch

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
@@ -14,38 +12,49 @@ let humanscore = 0;
 let computerscore = 0;
 
 function playRound(humanChoice, computerChoice) {
+    const resultdiv = document.querySelector("#result");
+    const scorediv = document.querySelector("#score");
+
+    let result = "";
     if(humanChoice.toLowerCase() === computerChoice.toLowerCase()) {
-        return "Tie";
+        result = "Tie";
+        humanscore++;
+        computerscore++;
+        console.log('tie');
     } else if(
         (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors") ||
         (humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock") ||
         (humanChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "paper")){
             humanscore++;
-            return "human wins";
+            result = "human wins";
+            console.log("human wins");
         } else {
             computerscore++;
-            return "computer wins";
+            result = "computer wins";
+            console.log("computer wins");
+    }
+
+    resultdiv.textContent = result;
+    scorediv.textContent = "human: " + humanscore + " - computer: " + computerscore;
+
+    if(humanscore === 5 || computerscore === 5) {
+        if(humanscore > computerscore) {
+            resultdiv.textContent = "human wins";
+        } else {
+            resultdiv.textContent = "computer wins";
         }
-}
-
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-console.log(playRound(humanChoice, computerChoice));
-
-
-function playGame(){
-    humanscore = 0; computerscore = 0;
-    for(let i = 0; i < 5; i++){
-        console.log("Round " + (i + 1));
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        console.log(playRound(humanChoice, computerChoice));
-    }
-    if(humanscore > computerscore) {
-        console.log("human wins");
-    } else{
-        console.log("computer wins");
+        alert("game over " + `${resultdiv.textContent}`);
+        humanscore = 0;
+        computerscore = 0;
     }
 }
 
-playGame();
+document.querySelector("#rock").addEventListener("click", function() {
+    playRound("rock", getComputerChoice());
+});
+document.querySelector("#paper").addEventListener("click", function() {
+    playRound("paper", getComputerChoice());
+});
+document.querySelector("#scissors").addEventListener("click", function() {
+    playRound("scissors", getComputerChoice());
+});
